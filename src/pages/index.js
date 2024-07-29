21175bd771dc0244612eed1510d63d1a4661a134
mithunshanker <87591451+mithunshanker@users.noreply.github.com>
2024-07-29 23:01:54 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,37 +24,26 @@ const logIn = async (e) => {
   <meta charset="UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"/>
-  <title>EVM Admin Dashboard</title>
+  <title>AVK EVM Login</title>
 </Head>
-    <div>
-    <nav class="navbar navbar-expand-lg bg-black navbar-dark border-bottom border-2 border-dark">
-  <div class="container-fluid my-2 justify-content-center">
-  <div class="hstack gap-3">
-  <a class="navbar-brand btn btn-transparent justify-center " href="#">
-            <img src="https://media.licdn.com/dms/image/C510BAQHjLBHoxIBfNg/company-logo_200_200/0/1580626235010?e=1695859200&v=beta&t=uMWv18NJ1_C9XrBAuGxd16Kq2XaIrr8ot62WPzgi9w0" class="rounded-circle" alt="AVK" width="68" height="68"/>
-        </a>
-  <div class="p-1">
+  
+
+<nav class="navbar navbar-expand-lg bg-black navbar-dark border-bottom border-1 border-dark mb-3">
+  <div class="container-fluid text-center my-2 ">
+    
+  <div class="p-1 mx-auto">
   <a class="navbar-brand" href="#">
-     <span class="fw-bolder font-monospace">Election Commision</span><br/>
+     <span class="fw-bolder font-monospace">Election Commission</span><br/>
      <span class="lead">Adarsh Vidya Kendra</span>  
     </a>
   </div>
-  <div class="vr border-dark text-secondary fw-bolder border-2"></div>
-  <div class="p-1">
-    <a class="nav-link text-white font-decoration-none" href="#">
-        <span class="fw-bolder font-monospace">Electronic Voting Machine</span><br/>
-        <span class="lead">Admin Portal Login</span>  
-    </a>
-  </div>
-</div>
 
 
+    
   </div>
 </nav>
 
 
-<div>
-  <div  >
 <div class="input-group input-group-lg justify-content-center py-4" data-bs-theme="dark">
 <div class="d-flex flex-column mb-3">
   <div class="p-0">
@@ -74,9 +63,13 @@ const logIn = async (e) => {
 </div>
 
 </div>
-</div>
-</div>
-    </div>
+
+<footer class="border-top border-dark border-1 py-2 fixed-bottom">
+  <center>
+  <p class="text-dark fw-semibold font-monospace">Developed by <span class="font-monospace text-secondary">AVK IT CELL</span></p>
+
+  </center>
+</footer>  
     </>
   )
 }
